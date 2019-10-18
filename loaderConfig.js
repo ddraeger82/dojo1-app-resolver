62fe0123925f1c8be0fbae98a35f5d3f1b3d8990
Draeger, Daniel <Daniel.Draeger@salt-solutions.de>
2019-10-18 15:18:10 +0200

    loaderConfig

--- loaderConfig.js
+++ loaderConfig.js
@@ -1,7 +1,7 @@
 // I don't think, that we need that
 dojoConfig = {
-	baseUrl: 'http://localhost:9998/myDojo1Widget/',
-	packages: [{ name: 'widgetDojo', location: 'dojo' }, { name: 'myDojo1Widget', location: 'myDojo1Widget' }],
+	baseUrl: 'http://localhost:3000/dojo/js/',
+	packages: [{ name: 'widgetDojo', location: 'lib/dojo' }, { name: 'myDojo1Widget', location: 'lib/app' }],
 	map: {
 		widgetDojo: maskWidgetMap,
 		myDojo1Widget: maskWidgetMap
