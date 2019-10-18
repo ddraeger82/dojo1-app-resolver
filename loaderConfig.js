fa725376b3204e65bfe36b3e1fa33da6d6885736
Draeger, Daniel <Daniel.Draeger@salt-solutions.de>
2019-10-18 15:20:59 +0200

    loaderConfig

--- loaderConfig.js
+++ loaderConfig.js
@@ -1,10 +1,14 @@
 // I don't think, that we need that
+let widgetMap = {
+	dojo: 'widgetDojo'
+};
+
 dojoConfig = {
 	baseUrl: 'http://localhost:3000/dojo/js/',
 	packages: [{ name: 'widgetDojo', location: 'lib/dojo' }, { name: 'myDojo1Widget', location: 'lib/app' }],
 	map: {
-		widgetDojo: maskWidgetMap,
-		myDojo1Widget: maskWidgetMap
+		widgetDojo: widgetMap,
+		myDojo1Widget: widgetMap
 	},
 	has: {
 		highcontrast: 0
